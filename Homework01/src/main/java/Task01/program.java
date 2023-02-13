45bf70918c63d4d4ffe674257714cfa601b9b59d
Valerii_Panov <val250640@mail.ru>
2023-02-13 11:15:34 +1000

    HM01

--- Homework01/src/main/java/Task01/program.java
+++ Homework01/src/main/java/Task01/program.java
@@ -1,7 +1,13 @@
 package Task01;
 //+Написать программу вычисления n-ого треугольного числа.
+import java.util.Scanner;
+
 public class program {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("n = ");
+        int n = scan.nextInt();
+        double t = 0.5*n*(n+1);
+        System.out.printf("Tn = %.0f", t);
     }
 }
