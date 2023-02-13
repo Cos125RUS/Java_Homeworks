fa87559a6782bf533a1f26a90dcdf6b78b32c903
Valerii_Panov <val250640@mail.ru>
2023-02-13 15:35:32 +1000

    task01

--- Homework02/Task01/program.java
+++ Homework02/Task01/program.java
@@ -9,60 +9,72 @@ package Task01;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 
 public class program {
+    static String[] sepor = {"name", "country", "city", "age"};
+    static int size = 4;
+
     public static void main(String[] args) {
         String title = "select * from students where ";
-//        StringBuilder request = new StringBuilder();
-//        request.append(title);
-        String student = readJSON().replace("{", "").
-                replace("}", "").trim();
-        System.out.println(student);
-        breaker(student);
+        String[] student = readJSON();
+        String[] values = getValue(student);
+        Map <String, String> pair = addPair(values);
+        String request = addRequest(pair, title);
+        System.out.println(request);
     }
 
-    public static String readJSON() {
+    public static String[] readJSON() {
         String path = "C:\\repo\\Java\\Homeworks\\Homework02\\Task01\\student.json";
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
-            StringBuilder addStudent = new StringBuilder();
+            String[] addStudent = new String[size];
             String line;
+            int i = 0;
             while ((line = reader.readLine()) != null) {
-                addStudent.append(line);
+                if (!line.equals("{") && !line.equals("}")){
+                    addStudent[i] = line.trim();
+                    i++;
+                }
             }
-            return addStudent.toString();
+            return addStudent;
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
         }
     }
+    public static String[] getValue(String[] student){
+        String[] values = new String[size];
+        for (int i = 0; i < size; i++) {
+            values[i] = student[i].replaceAll(sepor[i], "")
+                    .replaceAll(": ", "")
+                    .replace('"',' ').replace(',',' ').trim();
+        }
+        return values;
+    }
 
-    public static void breaker(String line) {
-//        Map<String, String> res = new HashMap<>();
-        String[] sepor = {"name", "country", "city", "age"};
-        for (String key: sepor) {
-            int index = line.lastIndexOf(key) + key.length() + 4;
-            int i = index;
-            while (line.charAt(i) != '"'){
-                System.out.print(line.charAt(i));
+    public static Map<String, String> addPair(String[] values){
+        Map<String, String> pair = new HashMap<>();
+        for (int i = 0; i < size; i++)
+            if (!values[i].equals("null"))
+                pair.put(sepor[i], values[i]);
+        return pair;
+    }
+
+    public static String addRequest(Map<String, String> pair, String title){
+        StringBuilder request = new StringBuilder();
+        request.append(title);
+        int size = pair.size();
+        int i = 0;
+        for (Entry<String, String> entry: pair.entrySet()) {
+            request.append(entry.getKey()).append(" = ")
+                    .append(entry.getValue());
+            if (i < size - 1){
+                request.append(" and ");
                 i++;
             }
-            char[] value = new char[i - index];
-            line.getChars(index, i, value, 0);
-            System.out.println(value.toString());
-//            res.put(key,value);
         }
-
-
-//        char[] text = line.toCharArray();
-//        int size = line.length();
-//        int i = 0;
-//        while (i< size){
-//            if (text[i] == ':'){
-//                System.out.println(i);
-//            }
-//            i++;
-//        }
-
-//        return res;
+        return request.toString();
     }
 }
