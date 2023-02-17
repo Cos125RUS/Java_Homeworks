a6b2f54def83d51b7e97754d8abe1151cd2031fe
Valerii_Panov <val250640@mail.ru>
2023-02-17 11:51:20 +1000

    Task01

--- Tasks/Task01/Task01.java
+++ Tasks/Task01/Task01.java
@@ -1,26 +1,31 @@
-package Tasks;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class Task01 {
     public static void main(String[] args) throws Exception{
-        Scanner scanner = new Scanner(System.in);
         Logger logger = Logger.getLogger(Task01.class.getName());
-        FileHandler fh = new FileHandler("src/main/java/Tasks/log.txt");
+        FileHandler fh = new FileHandler("log.txt");
         logger.addHandler(fh);
         SimpleFormatter txt = new SimpleFormatter();
         fh.setFormatter(txt);
 
+
+
         try {
-            System.out.print("a = ");
-            int a = scanner.nextInt();
-            System.out.print("b = ");
-            int b = scanner.nextInt();
+            BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+            int a = arg(br);
+            System.out.println(a);
+            int b = arg(br);
+            System.out.println(b);
             int res = math(a,b);
-            System.out.println("res = " + res);
+            FileWriter fr = new FileWriter("output.txt", false);
+            fr.write("res = " + res);
+            fr.flush();
             logger.info(a+"^"+b+" = "+res);
         }
         catch (Exception e){
@@ -37,5 +42,10 @@ public class Task01 {
         return res;
     }
 
+    static int arg(BufferedReader br) throws Exception{
+        String st = br.readLine();
+        String[] arr = st.split(" ");
+        return Integer.parseInt(arr[1]);
+    }
 
 }
