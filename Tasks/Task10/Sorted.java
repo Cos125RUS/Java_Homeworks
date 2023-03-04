96e8ee68949d006fb6c69b861cf9b2463cadf610
Valerii_Panov <val250640@mail.ru>
2023-03-04 20:21:34 +1000

    Task10v2.4

--- Tasks/Task10/Sorted.java
+++ Tasks/Task10/Sorted.java
@@ -1,42 +1,50 @@
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Sorted {
     public static void sort(Deque<Object> digits, Deque<String> operations){
-        checking(digits, operations, "*", "/");
-        checking(digits, operations, "^", "^");
+        Deque<Object> sortedDigits = new ArrayDeque<>();
+        Deque<String> sortedOperations = new ArrayDeque<>();
+        checking(digits, operations, sortedDigits, sortedOperations, "^", "^");
+        checking(digits, operations, sortedDigits, sortedOperations, "*", "/");
+        checking(digits, operations, sortedDigits, sortedOperations, "+", "-");
+        change(digits, operations, sortedDigits, sortedOperations);
     }
 
-    public static void checking(Deque<Object> digits, Deque<String> operations, String check1, String check2){
-        int count = 0;
-        Queue<Object> nums = new LinkedList<>();
-        Queue<String> operat = new LinkedList<>();
+    public static void checking(Deque<Object> digits, Deque<String> operations,
+                                Deque<Object> sortedDigits, Deque<String> sortedOperations,
+                                String check1, String check2){
 
         for (int i = 0; i < operations.size(); i++){
             Object a = digits.poll();
-            Object b = digits.poll();
             String op = operations.poll();
             if (op.equals(check1) || op.equals(check2)) {
-                count++;
-                nums.add(b);
-                nums.add(a);
-                operat.add(op);
+                if (sortedDigits.size() == 0) {
+                    sortedDigits.add(a);
+                    a = digits.poll();
+                }
+                sortedDigits.add(a);
+                sortedOperations.add(op);
             }
             else {
                 digits.add(a);
-                digits.addFirst(b);
                 operations.add(op);
             }
         }
-        Object first = digits.poll();
-        digits.add(first);
-
-        for (int i = 0; i < count; i++){
-            digits.addFirst(nums.poll());
-            digits.addFirst(nums.poll());
-            operations.addFirst(operat.poll());
+        if (sortedDigits.size() == 0) {
+            Object first = digits.poll();
+            digits.add(first);
         }
     }
 
+    public static void change(Deque<Object> digits, Deque<String> operations,
+                              Deque<Object> sortedDigits, Deque<String> sortedOperations){
+        while (sortedOperations.size() > 0){
+            digits.add(sortedDigits.poll());
+            operations.add(sortedOperations.poll());
+        }
+        digits.add(sortedDigits.poll());
+    }
 }
